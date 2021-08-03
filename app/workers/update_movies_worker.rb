7a0e5ea6a2d46dd63f1b7576b0c3cda1181d52d5
oliver.smith <oliver.smith@smartpension.co.uk>
2021-08-03 11:30:57 +0100

    Added tests for UpdateMovieWorker

--- app/workers/update_movies_worker.rb
+++ app/workers/update_movies_worker.rb
@@ -2,17 +2,16 @@
 
 require 'sidekiq-scheduler'
 
-ActiveRecord::Base.logger = Logger.new(STDOUT)
-
 class UpdateMoviesWorker
   include Sidekiq::Worker
   sidekiq_options queue: :movies, retry: false
 
   def perform
     Movie.all.each do |movie|
-      if movie.updated_at > 2.days.ago
+      if movie.updated_at < 2.days.ago
         movie.destroy
       else
+        puts "*********"
         Movies::Update.new(movie).call
       end
     end
