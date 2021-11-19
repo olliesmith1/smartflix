99eba67b1600b8ccc172a2b7289ff5b4378f0792
oliver.smith <oliver.smith@smartpension.co.uk>
2021-11-19 10:08:17 +0000

    Add table to index page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,11 +2,10 @@
 
 class MoviesController < ApplicationController
   def show
-    title = params[:title].titleize
-    @movie = Movie.find_by(title: title)
+    @movie = Movie.find_by(title: params[:title].titleize)
 
     if @movie
-      render json: @movie, status: :ok
+      render "show", status: :ok
     else
       render json: {
         error: "Movie with title #{title} not found. We have requested this to be added to our library."
@@ -16,7 +15,7 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Kaminari.paginate_array(Movie.all).page(params[:page]).per(3)
+    @movies = Kaminari.paginate_array(Movie.all).page(params[:page]).per(5)
     render "index"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  get '/movies/:title', to: 'movies#show'
-  get '/movies/', to: 'movies#index'
+  resources :movies
   mount Sidekiq::Web => '/sidekiq'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
